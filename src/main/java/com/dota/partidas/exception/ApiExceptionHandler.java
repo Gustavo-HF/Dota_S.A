ecd6d97f422ba2015630dc4caa0b945fe8232282
Gustavo Henrique Fiamoncini <gustavohf092006@gmail.com>
2026-03-23 21:25:01 -0300

    Feat: Projeto Finalizado

--- src/main/java/com/dota/partidas/exception/ApiExceptionHandler.java
+++ src/main/java/com/dota/partidas/exception/ApiExceptionHandler.java
@@ -10,8 +10,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ApiExceptionHandler {
 
     @ExceptionHandler(RequestException.class)
-    public ResponseEntity<?> handle(RequestException ex){
+    public ResponseEntity<?> handle(RequestException ex) {
         return ResponseEntity.badRequest().body(Map.of("error", ex.getErroCode(), "message", ex.getMessage()));
     }
-    
+
+    @ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(org.springframework.web.bind.MethodArgumentNotValidException ex) {
+        // Aqui pegamos todos os erros de todos os campos de uma vez
+        var erros = ex.getFieldErrors().stream()
+                .map(error -> Map.of(
+                "campo", error.getField(),
+                "mensagem", error.getDefaultMessage()
+        ))
+                .toList();
+
+        return ResponseEntity.badRequest().body(erros);
+    }
+
 }
